bf02ce6b700f96dcabda189a2ca3c9a045a0af36
John Vollmer <vollme22@uwm.edu>
2018-08-01 06:11:53 -0500

    search bar implemented in all layouts, search method in home controller

--- Capstone_/Controllers/HomeController.cs
+++ Capstone_/Controllers/HomeController.cs
@@ -16,8 +16,64 @@ namespace Capstone_.Controllers
 {
     public class HomeController : Controller
     {
+        ApplicationDbContext db = new ApplicationDbContext();
         //GET /home/index
-        public ActionResult Index()
+        public ActionResult Home()
+        {
+            return View("Index");
+        }
+
+        public ActionResult Index(string option, string searchString)
+        {
+            List<PersonalUser> personalUsers = new List<PersonalUser>();
+            List<Company> companies = new List<Company>();
+
+            if (option == "PersonalUsers")
+            {
+                foreach (var user in db.PersonalUsers)
+                {
+                    if (user.Name.Contains(searchString))
+                    {
+                        personalUsers.Add(user);
+                    }
+                    else if (user.Email.Contains(searchString))
+                    {
+                        personalUsers.Add(user);
+                    }
+                    else if (user.PhoneNumber.Contains(searchString))
+                    {
+                        personalUsers.Add(user);
+                    }
+                }
+                return View(personalUsers.ToList());
+            }
+            else if (option == "Companies")
+            {
+                foreach (var company in db.Companies)
+                {
+                    if (company.Name.Contains(searchString))
+                    {
+                        companies.Add(company);
+                    }
+                    else if (company.Email.Contains(searchString))
+                    {
+                        companies.Add(company);
+                    }
+                    else if (company.PhoneNumber.Contains(searchString))
+                    {
+                        companies.Add(company);
+                    }
+                    else if (company.Address.Contains(searchString))
+                    {
+                        companies.Add(company);
+                    }
+                }
+                return View(companies.ToList());
+            }
+            return View();
+        }
+
+        public ActionResult Search()
         {
             return View();
         }
---
+++ Capstone_/Controllers/SearchController.cs
@@ -0,0 +1,64 @@
+﻿using Capstone_.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Capstone_.Controllers
+{
+    public class SearchController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Search
+        public ActionResult Index(string option, string searchString)
+        {
+            List<PersonalUser> personalUsers = new List<PersonalUser>();
+            List<Company> companies = new List<Company>();
+
+            if (option == "PersonalUsers")
+            {
+                foreach (var user in db.PersonalUsers)
+                {
+                    if (user.Name.Contains(searchString))
+                    {
+                        personalUsers.Add(user);
+                    }
+                    else if (user.Email.Contains(searchString))
+                    {
+                        personalUsers.Add(user);
+                    }
+                    else if (user.PhoneNumber.Contains(searchString))
+                    {
+                        personalUsers.Add(user);
+                    }
+                }
+                return View(personalUsers.ToList());
+            }
+            else if (option == "Companies")
+            {
+                foreach (var company in db.Companies)
+                {
+                    if (company.Name.Contains(searchString))
+                    {
+                        companies.Add(company);
+                    }
+                    else if (company.Email.Contains(searchString))
+                    {
+                        companies.Add(company);
+                    }
+                    else if (company.PhoneNumber.Contains(searchString))
+                    {
+                        companies.Add(company);
+                    }
+                    else if (company.Address.Contains(searchString))
+                    {
+                        companies.Add(company);
+                    }
+                }
+                return View(companies.ToList());
+            }
+            return View();
+        }
+    }
+}
